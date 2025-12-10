a8419045944c4470fcbc1b763821ac869fc656a9
Ashrut Panchal <ashrutpanhal112@gmail.com>
2025-12-11 00:40:32 +0530

    New Icon of Aum

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
+export const metadata = {
+  title: "Shri PraveenNath Maharaj",
+  description:
+    "Official website of Shri PraveenNath Maharaj – spiritual guide from Indore",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
