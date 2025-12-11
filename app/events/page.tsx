c81e6abc01a5f8595980dc7adfa75aec10905112
Ashrut Panchal <ashrutpanhal112@gmail.com>
2025-12-11 13:36:45 +0530

    Updated the Event

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -7,9 +7,21 @@ export default function EventsPage() {
 
       <div className="space-y-4">
         <div className="bg-white border rounded-xl p-4">
-          <p className="font-medium">25 December 2025</p>
+          <p className="font-medium">11 December 2025</p>
           <p className="text-sm text-slate-600">
-            Satsang and discourse at Indore Ashram
+             सभी माता भक्तों को सूचित किया जाता है की,
+              दिनांक 11 दिसंबर 2025 गुरुवार को मां रेणुका का कुमकुम अर्चन पूजा सह गुरुवार उपासना
+              श्री अनंतजी नेवासकर के निवास पर रखी गई है।
+              जिनका निवास का पत्ता नीचे दिया गया है।
+              <br/>
+              समय शाम 5 बजे से।
+              <br/>
+
+              स्थान: मकान नंबर 1592 D सेक्टर
+              सुदामा नगर नेमा कुल्फी के सामने वाली गली
+              <br/>
+
+              मोबाइल: 9826150729
           </p>
         </div>
 
